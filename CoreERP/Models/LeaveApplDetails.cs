41000c7c95d562299207d193a495085a3b6f6444
krishnaprasadg@info-sun.com <krishnaprasadg@info-sun.com>
2020-02-18 11:27:54 +0530

    leave apply detail model chnages pankaj commit

--- CoreERP/Models/LeaveApplDetails.cs
+++ CoreERP/Models/LeaveApplDetails.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace CoreERP.Models
@@ -7,19 +7,17 @@ namespace CoreERP.Models
     {
         public string EmpCode { get; set; }
         public string EmpName { get; set; }
-        public int Sno { get; set; }
+        public int? Sno { get; set; }
         public DateTime? ApplDate { get; set; }
         public string LeaveCode { get; set; }
         public DateTime? LeaveFrom { get; set; }
         public DateTime? LeaveTo { get; set; }
         public double? LeaveDays { get; set; }
-        //public double? CountofLeaves { get; set; }
-       public string LeaveRemarks { get; set; }
+        public string LeaveRemarks { get; set; }
         public string Status { get; set; }
         public string ApprovedId { get; set; }
         public string ApprovedName { get; set; }
         public string Reason { get; set; }
-        //public string chkAcceptReject { get; set; }
         public string RecommendedId { get; set; }
         public string RecommendedName { get; set; }
         public DateTime? ApprovedDate { get; set; }
