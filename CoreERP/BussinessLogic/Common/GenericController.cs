2a735d764369909f80a4d300dd30123d623b4293
KrishnaPrasad.Gedda@xpo.com <KrishnaPrasad.Gedda@xpo.com>
2020-08-19 21:05:21 +0530

    Added Transaction Forms

---
+++ CoreERP/BussinessLogic/Common/GenericController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreERP.BussinessLogic.Common
+{
+    public class GenericController
+    {
+    }
+
+    public class ControllerServiceProvider : IApplicationFeatureProvider<ControllerFeature>
+    {
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+        {
+            foreach( var partType in parts.OfType<IApplicationPartTypeProvider>())
+            {
+                foreach(var part in partType.Types)
+                {
+                    if (part.IsSubclassOf(typeof(Controller)) || feature.Controllers.Contains(part))
+                    {
+                        feature.Controllers.Add(part);
+                    }
+                }
+            }
+        }
+    }
+
+    //public class ActionSerivceProvider
+    //{
+    //    public static void ProcessAction(Type type, IRepository<TblLanguage> repository)
+    //    {
+
+    //    }
+    //}
+}
---
+++ CoreERP/BussinessLogic/Common/InputWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreERP.BussinessLogic.Common
+{
+
+    public enum ObjectState
+    {
+        New,
+        Update,
+        Delete        
+    }
+
+    public class InputWrapper
+    {
+        /// <summary>
+        /// Indicates whether object is newly created, updated or marked for delete
+        /// </summary>
+        public ObjectState State { set; get; }
+        /// <summary>
+        /// The model entity such as TblCurrency
+        /// </summary>
+        public object Entity { set; get; }
+        /// <summary>
+        /// model entity type such as TblCurrency
+        /// </summary>
+        public string EType { set; get; }
+    }
+}
