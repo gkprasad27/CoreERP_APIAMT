56e9976577da38f254258f0d8c0c67ef162db99a
krishnaprasadg <krishnaprasadg@ITLWS016>
2023-08-15 12:07:18 +0530

    Issues Fixed

--- CoreERP/Models/TblCashBankMaster.cs
+++ CoreERP/Models/TblCashBankMaster.cs
@@ -5,28 +5,28 @@ namespace CoreERP.Models
 {
     public partial class TblCashBankMaster
     {
-        public string Company { get; set; }
-        public string Branch { get; set; }
-        public string VoucherClass { get; set; }
-        public string VoucherType { get; set; }
+        public string? Company { get; set; }
+        public string? Branch { get; set; }
+        public string? VoucherClass { get; set; }
+        public string? VoucherType { get; set; }
         public DateTime? VoucherDate { get; set; }
         public DateTime? PostingDate { get; set; }
         public DateTime? Period { get; set; }
-        public string VoucherNumber { get; set; }
-        public string TransactionType { get; set; }
-        public string NatureofTransaction { get; set; }
-        public string Account { get; set; }
-        public string AccountingIndicator { get; set; }
-        public string ReferenceNo { get; set; }
+        public string? VoucherNumber { get; set; }
+        public string? TransactionType { get; set; }
+        public string? NatureofTransaction { get; set; }
+        public string? Account { get; set; }
+        public string? AccountingIndicator { get; set; }
+        public string? ReferenceNo { get; set; }
         public DateTime? ReferenceDate { get; set; }
-        public string ProfitCenter { get; set; }
-        public string Segment { get; set; }
-        public string Narration { get; set; }
-        public string Status { get; set; }
-        public string Ext { get; set; }
-        public string AddWho { get; set; }
+        public string? ProfitCenter { get; set; }
+        public string? Segment { get; set; }
+        public string? Narration { get; set; }
+        public string? Status { get; set; }
+        public string? Ext { get; set; }
+        public string? AddWho { get; set; }
         public DateTime? AddDate { get; set; }
-        public string EditWho { get; set; }
+        public string? EditWho { get; set; }
         public DateTime? EditDate { get; set; }
         public decimal? TotalAmount { get; set; }
     }
