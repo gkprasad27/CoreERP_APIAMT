1c2b060fba8fe3fab6f55f1b71cf54d3750e7b5a
krishnaprasadg <krishnaprasadg@ITLWS016>
2023-08-16 13:02:15 +0530

    Issues Fixed

--- CoreERP/Models/TbBommaster.cs
+++ CoreERP/Models/TbBommaster.cs
@@ -5,15 +5,15 @@ namespace CoreERP.Models
 {
     public partial class TbBommaster
     {
-        public string Company { get; set; }
-        public string Plant { get; set; }
-        public string Bomtype { get; set; }
-        public string Bomnumber { get; set; }
-        public string Description { get; set; }
-        public string CostUnit { get; set; }
-        public string Material { get; set; }
-        public string Batch { get; set; }
-        public string CreatedBy { get; set; }
-        public string LevelType { get; set; }
+        public string? Company { get; set; }
+        public string? Plant { get; set; }
+        public string? Bomtype { get; set; }
+        public string? Bomnumber { get; set; }
+        public string? Description { get; set; }
+        public string? CostUnit { get; set; }
+        public string? Material { get; set; }
+        public string? Batch { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LevelType { get; set; }
     }
 }
--- CoreERP/Models/TblBomDetails.cs
+++ CoreERP/Models/TblBomDetails.cs
@@ -5,15 +5,16 @@ namespace CoreERP.Models
 {
     public partial class TblBomDetails
     {
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedAttribute(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public string BomKey { get; set; }
-        public string BomLevel { get; set; }
-        public string Component { get; set; }
-        public string Description { get; set; }
-        public string ManufacturingType { get; set; }
-        public string Type { get; set; }
-        public string AboveLevel { get; set; }
+        public string? BomKey { get; set; }
+        public string? BomLevel { get; set; }
+        public string? Component { get; set; }
+        public string? Description { get; set; }
+        public string? ManufacturingType { get; set; }
+        public string? Type { get; set; }
+        public string? AboveLevel { get; set; }
         public int? Qty { get; set; }
-        public string Uom { get; set; }
+        public string? Uom { get; set; }
     }
 }
