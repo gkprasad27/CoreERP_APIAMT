4bda66d933866400fe55e438424e02aa5b79a8b7
krishnaprasadg <krishnaprasadg@ITLWS016>
2023-08-16 16:08:31 +0530

    Issues Fixed

--- CoreERP/Models/TblInspectionCheckDetails.cs
+++ CoreERP/Models/TblInspectionCheckDetails.cs
@@ -6,13 +6,13 @@ namespace CoreERP.Models
     public partial class TblInspectionCheckDetails
     {
         public int Id { get; set; }
-        public string InspectionCheckNo { get; set; }
-        public string MaterialCode { get; set; }
-        public string Description { get; set; }
+        public string? InspectionCheckNo { get; set; }
+        public string? MaterialCode { get; set; }
+        public string? Description { get; set; }
         public int? ReceivedQty { get; set; }
         public int? RejectedQty { get; set; }
-        public string Location { get; set; }
-        public string MovementTo { get; set; }
-        public string RejectReason { get; set; }
+        public string? Location { get; set; }
+        public string? MovementTo { get; set; }
+        public string? RejectReason { get; set; }
     }
 }
