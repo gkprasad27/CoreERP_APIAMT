32f707f6b5f08b08f7a37adc4520e3b20a8718a1
krishnaprasadg <krishnaprasadg@ITLWS016>
2023-08-23 12:54:28 +0530

    Issues Fixed

--- CoreERP/Models/CostCenters.cs
+++ CoreERP/Models/CostCenters.cs
@@ -5,22 +5,27 @@ namespace CoreERP.Models
 {
     public partial class CostCenters
     {
-        public string Code { get; set; }
-        public string CompCode { get; set; }
-        public string Name { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
-        public string Address4 { get; set; }
-        public string Place { get; set; }
-        public string State { get; set; }
-        public string PinCode { get; set; }
-        public string Phone { get; set; }
-        public string Mobile { get; set; }
-        public string Ext { get; set; }
-        public string Email { get; set; }
-        public string ResponsiblePerson { get; set; }
-        public string Active { get; set; }
+        public string? Code { get; set; }
+        public string? CompCode { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? objectType { get; set; }
+        public string? functions { get; set; }
+        public string? Place { get; set; }
+        public string? State { get; set; }
+        public string? PinCode { get; set; }
+        public string? Phone { get; set; }
+        public string? Mobile { get; set; }
+        public string? CostObject { get; set; }
+        public string? Email { get; set; }
+        public string? ResponsiblePerson { get; set; }
+        public string? Active { get; set; }
         public DateTime? AddDate { get; set; }
+        public string? Department { get; set; }
+        public DateTime? FromDate { get; set; }
+        public string? Location { get; set; }
+        public string? Quality { get; set; }
+        public string? Type { get; set; }
     }
 }
