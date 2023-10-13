eb9e8ed8655bb45612bb5cd039167fdbb0640309
krishnaprasadg <krishnaprasadg@ITLWS016>
2023-10-13 23:54:23 +0530

    FileDownload Changes

--- CoreERP/Controllers/GeneralLedger/TransactionsController.cs
+++ CoreERP/Controllers/GeneralLedger/TransactionsController.cs
@@ -1275,25 +1275,39 @@ namespace CoreERP.Controllers.masters
             if (filename == null)
                 return Content("filename not present");
 
-            var path = Path.Combine(Directory.GetCurrentDirectory(), filename+".pdf");
-            var memory = new MemoryStream();
-            using (var stream = new FileStream(path, FileMode.Open))
+            string downloadURL = "ftp://amtpowertransmission.com/portal.amtpowertransmission.com/Doc/SaleOrder/" + filename + ".pdf";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(downloadURL);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential("amtpowertransm", "Zxan44*6");
+            request.UsePassive = true;
+            request.UseBinary = true;
+            request.KeepAlive = false;
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory() +"\\"+ filename + ".pdf");
+            Directory.CreateDirectory(Path.GetDirectoryName(pathToSave));
+            FileStream file = System.IO.File.Create(pathToSave);
+            byte[] buffer = new byte[2 * 1024];
+            int read;
+            while ((read = responseStream.Read(buffer, 0, buffer.Length)) > 0) { file.Write(buffer, 0, read); }
+            file.Close();
+            responseStream.Close();
+            response.Close();
+            if (System.IO.File.Exists(pathToSave))
             {
-                await stream.CopyToAsync(memory);
-            }
-            memory.Position = 0;
-            if (System.IO.File.Exists(path))
-            {
-                //byte[] imageBytes = System.IO.File.ReadAllBytes(path);
-                //string base64String = Convert.ToBase64String(imageBytes);
-                //var filepaths = "data:image/png;base64," + base64String;
-                string filePath = path;
+                string filePath = pathToSave;
                 byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
                 File(fileBytes, "application/force-download", filename);
-                return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = File(fileBytes, "application/force-download", filename) });
-            }
 
-            return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = path });
+                if (System.IO.File.Exists(pathToSave))
+                    System.IO.File.Delete(pathToSave);
+
+                //return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = File(fileBytes, "application/force-download", filename) });
+                return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = fileBytes });
+            }
+            return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = "fileBytes" });
         }
 
        
