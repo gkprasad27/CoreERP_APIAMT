b70a148b0adceba6cfc38dd2868991f34f318afa
krishnaprasadg <krishnaprasadg@ITLWS016>
2024-01-23 19:26:33 +0530

    Issues Fixed

---
+++ CoreERP/Controllers/masters/CapaController.cs
@@ -0,0 +1,112 @@
+﻿using CoreERP.DataAccess;
+using CoreERP.DataAccess.Repositories;
+using CoreERP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Dynamic;
+using System.Linq;
+
+namespace CoreERP.Controllers.masters
+{
+    [ApiController]
+    [Route("api/Capa")]
+    public class CapaController : Controller
+    {
+        private readonly IRepository<TblCAPA> _capaRepository;
+        public CapaController(IRepository<TblCAPA> CapaRepository)
+        {
+            _capaRepository = CapaRepository;
+        }
+
+        [HttpPost("Registercapa")]
+        public IActionResult RegisterDispatch([FromBody] TblCAPA capa)
+        {
+            if (capa == null)
+                return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = $"{nameof(capa)} cannot be null" });
+
+            try
+            {
+                APIResponse apiResponse;
+                _capaRepository.Add(capa);
+                if (_capaRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = capa };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Recored Added Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpGet("GetCapaList")]
+        public IActionResult GetCapaList()
+        {
+            try
+            {
+                var CapaList = _capaRepository.GetAll(); 
+                if (CapaList.Count() > 0)
+                {
+                    dynamic expdoObj = new ExpandoObject();
+                    expdoObj.CapaList = CapaList;
+                    return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = expdoObj });
+                }
+                else
+                    return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = "No Data Found." });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpPut("UpdateCapa")]
+        public IActionResult UpdateCapa([FromBody] TblCAPA capa)
+        {
+            if (capa == null)
+                return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = $"{nameof(capa)} cannot be null" });
+
+            try
+            {
+                APIResponse apiResponse;
+                _capaRepository.Update(capa);
+                if (_capaRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = capa };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Updation Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpDelete("Deletecapa/{code}")]
+        public IActionResult Deletecapa(int code)
+        {
+            try
+            {
+                APIResponse apiResponse;
+                if (code == null)
+                    return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = $"{nameof(code)} cannot be null" });
+                var record = _capaRepository.GetSingleOrDefault(x => x.ID.Equals(code));
+                _capaRepository.Remove(record);
+                if (_capaRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = record };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Deletion Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+    }
+}
---
+++ CoreERP/Models/TblCAPA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreERP.Models
+{
+    [Table("tbl_CAPA")]
+    public partial class TblCAPA
+    {
+        public int ID { get; set; }
+        public DateTime? DeadlineMonitorDate { get; set; }
+        public DateTime? ReceivedPartsDate { get; set; }
+        public DateTime? analysisBugunDate { get; set; }
+        public DateTime? DefectCauseAcknowledgeDate { get; set; }
+        public DateTime? ShortTermSolDate { get; set; }
+        public DateTime? longTermSolDate { get; set; }
+        public DateTime? ContainmentReqdDate { get; set; }
+        public DateTime? SolutionInspectedDate { get; set; }
+        public string? NotificationNo { get; set; }
+        public string? ItemCode { get; set; }
+        public string MaterialDescription { get; set; }
+        public DateTime? NotificationDate { get; set; }
+        public string? ComplaintQty { get; set; }
+        public string? Tag { get; set; }
+        public string? AddWho { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? AddDate { get; set; }
+
+        public string? DecfectDescription { get; set; }
+        public string? DefectCasuseResp { get; set; }
+        public string? ShortTermSolution { get; set; }
+        public string? ShortTermSolutionResp { get; set; }
+        public DateTime? ShortTermSolutionDate { get; set; }
+        public string? LongTermSolution { get; set; }
+        public string? LongTermSolutionResp { get; set; }
+        public DateTime? LongTermSolutionDate { get; set; }
+        public string? IdentificationOfDelvrdMaterial { get; set; }
+        public string? IdentificationOfDelvrdMaterialResp { get; set; }
+        public DateTime? IdentificationOfDelvrdMaterialDate { get; set; }
+        public string? CheckSuccessDelrdMaterail { get; set; }
+        public string? CheckSuccessDelrdMaterailResp { get; set; }
+        public DateTime? CheckSuccessDelrdMaterailDate { get; set; }
+        public string? ContainmentAccess { get; set; }
+        public string? ContainmentAccessResp { get; set; }
+        public DateTime? ContainmentAccessDate { get; set; }
+        public string? CustVerification { get; set; }
+        public DateTime? CustVerificationDate { get; set; }
+        public string? CAPANUM { get; set; }
+        public DateTime? DefectCasuseDate { get; set; }
+        public string? SaleOrderNo { get; set; }
+        public string? Status { get; set; }
+
+    }
+}
