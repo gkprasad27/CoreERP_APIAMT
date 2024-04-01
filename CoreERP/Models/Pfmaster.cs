7591a37ed7cc8c4175ac851ef9b83513fec75814
Padma <padmasree.madala@gmail.com>
2024-04-01 14:20:16 +0530

    CTC Breakup and PF master Screen changes

--- CoreERP/Models/Pfmaster.cs
+++ CoreERP/Models/Pfmaster.cs
@@ -8,13 +8,14 @@ namespace CoreERP.Models
         public int Id { get; set; }
         public string? CompanyCode { get; set; }
         public string? BranchCode { get; set; }
-        public string? PftypeName { get; set; }
+        public string? PfName { get; set; }
         public string? Limit { get; set; }
-        public string? ComponentCode { get; set; }
+        public string? PfType { get; set; }
         public string? ComponentName { get; set; }
         public double? EmployeeContribution { get; set; }
         public double? EmployerContribution { get; set; }
         public string? ContributionType { get; set; }
         public string? Active { get; set; }
+        public double? Amount { get; set; }
     }
 }
