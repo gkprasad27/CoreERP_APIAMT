17a1d6cab70d7172ce76eaf0d3c9960c973dd13f
krishnaprasadg <krishnaprasadg@ITLWS016>
2024-04-15 11:06:33 +0530

    Issues Fixed

--- CoreERP/Controllers/masters/CommonController.cs
+++ CoreERP/Controllers/masters/CommonController.cs
@@ -282,7 +282,7 @@ namespace CoreERP.Controllers
                 try
                 {
                     dynamic expando = new ExpandoObject();
-                    expando.BPList = _somRepository.Where(x => (x.Status == "Material Received" || x.Status == "Material Partial Received" || x.Status == "SO Created" && x.Company == CompanyCode)).Select(x => new { saleOrderNo = x.SaleOrderNo });
+                    expando.BPList = _somRepository.Where(x => (x.Status == "Material Received" || x.Status == "Material Partial Received" || x.Status == "SO Created"||x.Status== "Partial PO Created" && x.Company == CompanyCode)).Select(x => new { saleOrderNo = x.SaleOrderNo });
                     return Ok(new APIResponse() { status = APIStatus.PASS.ToString(), response = expando });
                 }
                 catch (Exception ex)
