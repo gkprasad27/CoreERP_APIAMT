d78d25498bdd2bc0ea4afe7d0a7eb9c46cc42bce
Padma <padmasree.madala@gmail.com>
2024-04-25 06:59:50 +0530

    Payslip print changes

--- CoreERP/BussinessLogic/ReportsHelpers/ReportsHelperClass.cs
+++ CoreERP/BussinessLogic/ReportsHelpers/ReportsHelperClass.cs
@@ -312,6 +312,38 @@ namespace CoreERP.BussinessLogic.ReportsHelpers
             else return null;
         }
 
+        public static DataSet GetPayslip(DateTime fromDate, DateTime toDate, string company)
+        {
+            ScopeRepository scopeRepository = new ScopeRepository();
+            using DbCommand command = scopeRepository.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "GetSalaryDetailsPivot";
+            #region Parameters
+
+            DbParameter pmfDate = command.CreateParameter();
+            pmfDate.Direction = ParameterDirection.Input;
+            pmfDate.Value = String.Format("{0:MM/dd/yyyy}", fromDate);
+            pmfDate.ParameterName = "fromdate";
+
+            DbParameter pmtDate = command.CreateParameter();
+            pmtDate.Direction = ParameterDirection.Input;
+            pmtDate.Value = String.Format("{0:MM/dd/yyyy}", toDate);
+            pmtDate.ParameterName = "todate";
+
+            DbParameter companyid = command.CreateParameter();
+            companyid.Direction = ParameterDirection.Input;
+            companyid.Value = (object)company ?? DBNull.Value;
+            companyid.ParameterName = "compcode";
+            #endregion
+            // Add parameter as specified in the store procedure
+
+            command.Parameters.Add(pmfDate);
+            command.Parameters.Add(pmtDate);
+            command.Parameters.Add(companyid);
+            return scopeRepository.ExecuteParamerizedCommand(command);
+        }
+
+
         public static DataTable GetEmpPresent(string company)
         {
             ScopeRepository scopeRepository = new ScopeRepository();
