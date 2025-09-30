213dbcc8dee3ecdc3bb2f6f52371c85acc6cac30
gkprasad27 <krishnaprasadg81@gmail.com>
2025-09-30 23:03:31 +0530

    EmployeeControlChanges

--- CoreERP/Controllers/masters/EmployeeController.cs
+++ CoreERP/Controllers/masters/EmployeeController.cs
@@ -14,9 +14,15 @@ namespace CoreERP.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IRepository<TblEmployee> _employeeRepository;
-        public EmployeeController(IRepository<TblEmployee> employeeRepository)
+        private readonly IRepository<TblAddress> _addressRepository;
+        private readonly IRepository<TblEducation> _edicationRepository;
+        private readonly IRepository<TblExperiance> _experianceRepository;
+        public EmployeeController(IRepository<TblEmployee> employeeRepository, IRepository<TblAddress> addressRepository, IRepository<TblEducation> educationRepository, IRepository<TblExperiance> experianceRepository)
         {
             _employeeRepository = employeeRepository;
+            _addressRepository = addressRepository;
+            _edicationRepository = educationRepository;
+            _experianceRepository = experianceRepository;
         }
 
         [HttpGet("GetEmployeeList/{companycode}")]
@@ -32,6 +38,83 @@ namespace CoreERP.Controllers
                     {
                         dynamic expdoObj = new ExpandoObject();
                         expdoObj.employeesList = employeesList;
+                        return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = expdoObj });
+                    }
+                    else
+                        return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = "No Data Found for branches." });
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+            });
+            return result;
+        }
+
+        [HttpGet("GetAddressList/{empcode}")]
+
+        public async Task<IActionResult> GetAddressList(string empcode)
+        {
+            var result = await Task.Run(() =>
+            {
+                try
+                {
+                    var AddressList = _addressRepository.GetAll().Where(x=>x.EmpCode==empcode); //LanguageHelper.GetList();
+                    if (AddressList.Count() > 0)
+                    {
+                        dynamic expdoObj = new ExpandoObject();
+                        expdoObj.AddressList = AddressList;
+                        return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = expdoObj });
+                    }
+                    else
+                        return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = "No Data Found for branches." });
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+            });
+            return result;
+        }
+
+        [HttpGet("GetEducationList/{empcode}")]
+
+        public async Task<IActionResult> GetEducationList(string empcode)
+        {
+            var result = await Task.Run(() =>
+            {
+                try
+                {
+                    var EducationList = _edicationRepository.GetAll().Where(x => x.EmpCode == empcode); //LanguageHelper.GetList();
+                    if (EducationList.Count() > 0)
+                    {
+                        dynamic expdoObj = new ExpandoObject();
+                        expdoObj.EducationList = EducationList;
+                        return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = expdoObj });
+                    }
+                    else
+                        return Ok(new APIResponse { status = APIStatus.FAIL.ToString(), response = "No Data Found for branches." });
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+            });
+            return result;
+        }
+        [HttpGet("GetExperianceList/{empcode}")]
+
+        public async Task<IActionResult> GetExperianceList(string empcode)
+        {
+            var result = await Task.Run(() =>
+            {
+                try
+                {
+                    var ExperianceList = _experianceRepository.GetAll().Where(x => x.EmpCode == empcode); //LanguageHelper.GetList();
+                    if (ExperianceList.Count() > 0)
+                    {
+                        dynamic expdoObj = new ExpandoObject();
+                        expdoObj.ExperianceList = ExperianceList;
                         return Ok(new APIResponse { status = APIStatus.PASS.ToString(), response = expdoObj });
                     }
                     else
@@ -72,7 +155,86 @@ namespace CoreERP.Controllers
             }
         }
 
+        [HttpPost("RegisterEmployeeAddress")]
 
+        public async Task<IActionResult> RegisterEmployeeAddress([FromBody] TblAddress address)
+        {
+            APIResponse apiResponse;
+            if (address == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(address)} cannot be null" });
+            else
+            {
+                try
+                {
+                    _addressRepository.Add(address);
+                    if (_addressRepository.SaveChanges() > 0)
+                        apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = address };
+                    else
+                        apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Registration Failed." };
+
+                    return Ok(apiResponse);
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+
+            }
+        }
+
+        [HttpPost("RegisterEducation")]
+
+        public async Task<IActionResult> RegisterEducation([FromBody] TblEducation education)
+        {
+            APIResponse apiResponse;
+            if (education == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(education)} cannot be null" });
+            else
+            {
+                try
+                {
+                    _edicationRepository.Add(education);
+                    if (_edicationRepository.SaveChanges() > 0)
+                        apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = education };
+                    else
+                        apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Registration Failed." };
+
+                    return Ok(apiResponse);
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+
+            }
+        }
+
+        [HttpPost("RegisterExperiance")]
+
+        public async Task<IActionResult> RegisterExperiance([FromBody] TblExperiance experiance)
+        {
+            APIResponse apiResponse;
+            if (experiance == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(experiance)} cannot be null" });
+            else
+            {
+                try
+                {
+                    _experianceRepository.Add(experiance);
+                    if (_experianceRepository.SaveChanges() > 0)
+                        apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = experiance };
+                    else
+                        apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Registration Failed." };
+
+                    return Ok(apiResponse);
+                }
+                catch (Exception ex)
+                {
+                    return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+                }
+
+            }
+        }
         [HttpPut("UpdateEmployee")]
 
         public async Task<IActionResult> UpdateEmployee([FromBody] TblEmployee employee)
@@ -97,7 +259,77 @@ namespace CoreERP.Controllers
             }
         }
 
+        [HttpPut("UpdateAddress")]
 
+        public async Task<IActionResult> UpdateAddress([FromBody] TblAddress address)
+        {
+            APIResponse apiResponse = null;
+            if (address == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Request cannot be null" });
+
+            try
+            {
+                _addressRepository.Update(address);
+                if (_addressRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = address };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Updation Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpPut("UpdateEducation")]
+
+        public async Task<IActionResult> UpdateEducation([FromBody] TblEducation education)
+        {
+            APIResponse apiResponse = null;
+            if (education == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Request cannot be null" });
+
+            try
+            {
+                _edicationRepository.Update(education);
+                if (_edicationRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = education };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Updation Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpPut("UpdateExperiance")]
+
+        public async Task<IActionResult> UpdateExperiance([FromBody] TblExperiance experiance)
+        {
+            APIResponse apiResponse = null;
+            if (experiance == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Request cannot be null" });
+
+            try
+            {
+                _experianceRepository.Update(experiance);
+                if (_experianceRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = experiance };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Updation Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
         //// Delete Branch
         [HttpDelete("DeleteEmployee/{code}")]
 
@@ -112,6 +344,82 @@ namespace CoreERP.Controllers
                 var record = _employeeRepository.GetSingleOrDefault(x => x.EmployeeCode.Equals(code));
                 _employeeRepository.Remove(record);
                 if (_employeeRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = record };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Deletion Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpDelete("DeleteAddress/{code}")]
+
+        public async Task<IActionResult> DeleteAddress(string code)
+        {
+            APIResponse apiResponse = null;
+            if (code == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(code)}can not be null" });
+
+            try
+            {
+                var record = _addressRepository.GetSingleOrDefault(x => x.EmpCode.Equals(code));
+                _addressRepository.Remove(record);
+                if (_addressRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = record };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Deletion Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+
+        [HttpDelete("DeleteEducation/{code}")]
+
+        public async Task<IActionResult> DeleteEducation(string code)
+        {
+            APIResponse apiResponse = null;
+            if (code == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(code)}can not be null" });
+
+            try
+            {
+                var record = _edicationRepository.GetSingleOrDefault(x => x.EmpCode.Equals(code));
+                _edicationRepository.Remove(record);
+                if (_edicationRepository.SaveChanges() > 0)
+                    apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = record };
+                else
+                    apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Deletion Failed." };
+
+                return Ok(apiResponse);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = ex.Message });
+            }
+        }
+
+        [HttpDelete("DeleteExperiance/{code}")]
+
+        public async Task<IActionResult> DeleteExperiance(string code)
+        {
+            APIResponse apiResponse = null;
+            if (code == null)
+                return Ok(new APIResponse() { status = APIStatus.FAIL.ToString(), response = $"{nameof(code)}can not be null" });
+
+            try
+            {
+                var record = _experianceRepository.GetSingleOrDefault(x => x.EmpCode.Equals(code));
+                _experianceRepository.Remove(record);
+                if (_experianceRepository.SaveChanges() > 0)
                     apiResponse = new APIResponse() { status = APIStatus.PASS.ToString(), response = record };
                 else
                     apiResponse = new APIResponse() { status = APIStatus.FAIL.ToString(), response = "Deletion Failed." };
