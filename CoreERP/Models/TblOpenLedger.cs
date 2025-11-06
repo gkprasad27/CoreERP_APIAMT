39c289cb0a24c95d7b105d8037b1129492074745
gkprasad27 <krishnaprasadg81@gmail.com>
2025-11-06 12:28:59 +0530

    Issues Fixed

--- CoreERP/Models/TblOpenLedger.cs
+++ CoreERP/Models/TblOpenLedger.cs
@@ -6,9 +6,14 @@ namespace CoreERP.Models
     public partial class TblOpenLedger
     {
         public int Id { get; set; }
+        public string? CompCode { get; set; }
         public string? LedgerKey { get; set; }
         public string? FinancialYearStartFrom { get; set; }
         public string? FinancialYearEndTo { get; set; }
         public string? AccountingYear { get; set; }
+        public string? AddWho { get; set; }
+        public string? EditWho { get; set; }
+        public DateTime? AddDate { get; set; }
+        public DateTime? EditDate { get; set; }
     }
 }
