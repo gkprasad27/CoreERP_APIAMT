071b42c290fdccde6853f8e949434f1520511939
gkprasad27 <krishnaprasadg81@gmail.com>
2026-02-01 14:47:29 +0530

    OP Balac

--- CoreERP/Models/TblOpeningBalance.cs
+++ CoreERP/Models/TblOpeningBalance.cs
@@ -8,18 +8,18 @@ namespace CoreERP.Models
         public int? OpeningBalanceId { get; set; }
         public string CompanyCode { get; set; }
         public string VoucherNo { get; set; }
-        public DateTime? OpeningBalanceDate { get; set; }
+        public DateTime OpeningBalanceDate { get; set; }
         public string LedgerId { get; set; }
         public string LedgerCode { get; set; }
         public string LedgerName { get; set; }
         public string PaymentTypeId { get; set; }
         public string PaymentTypeName { get; set; }
-        public decimal? OpeningBalance { get; set; }
+        public decimal OpeningBalance { get; set; }
         public string Narration { get; set; }
         public string AddWho { get; set; }
         public string EditWho { get; set; }
         public DateTime AddDate { get; set; }
         public DateTime EditDate { get; set; }
-        public decimal? ClosingBalance { get; set; }
+        public decimal ClosingBalance { get; set; }
     }
 }
